8d37b6dcbe7fa762e5184eeff883d5eb3ec01841
nazishzaman <168236857+nazishzaman@users.noreply.github.com>
2024-11-05 07:15:11 -0800

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const Contact = () =>{
+    return(
+        <>
+        <link href="/">Contact us</link>
+        <h1>Welcome to home page</h1>
+        <button/>
+        <Contact/>
+        </>
+    );
+}
+
+export default Contact
